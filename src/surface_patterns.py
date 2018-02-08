a53851596ac034b96003025092d20b2e1dfa4e67
Maximilian Blunck <blunck@cl.uni-heidelberg.de>
2018-02-08 13:43:45 +0100

    Updated f3 with class

--- src/surface_patterns.py
+++ src/surface_patterns.py
@@ -1,8 +1,23 @@
 from collections import Counter
-import corpus
-from ngram_feature import get_vocabulary, extract
+from ngram_feature import NgramFeature
+import config
 
 
+class SurfacePatternFeature(NgramFeature):
+	"""
+	Class representing feature f3
+
+	extract-method returns a feature-vector of length of its vocabulary 
+	containing surface-pattern-n-gram counts
+
+	"""
+
+	corpus_key = 'SURFACE_PATTERNS'
+	n_range = config.n_range_surface_patterns
+	name = "Surface Pattern"
+
+
+# below are functions for creating surface patterns when loading corpus
 def extract_surface_patterns(corpus, frequency_per_million):
 	tokens = get_tokens_lower(corpus)
 	frequencies = frequency_breakdown(tokens)
@@ -19,9 +34,11 @@ def get_tokens_lower(corpus):
 			tokens.append(token.lower())
 	return tokens
 
+
 def frequency_breakdown(tokens):	
 	frequencies = Counter(tokens)
 	return frequencies
+
 
 def frequency_threshold(tokens, frequency_per_million):
 	length = len(tokens)
@@ -29,12 +46,14 @@ def frequency_threshold(tokens, frequency_per_million):
 	threshold = int(ratio*frequency_per_million)
 	return threshold
 
+
 def get_high_frequency_words(frequencies, threshold):
 	hfws = []
 	for key, value in frequencies.items():
 		if value >= threshold:
 			hfws.append(key)
 	return sorted(hfws)
+
 
 def substitute_content_words(corpus, hfws):
 	extended_corpus = corpus
@@ -46,10 +65,3 @@ def substitute_content_words(corpus, hfws):
 			sub_tokens.append(token)
 		instance["SURFACE_PATTERNS"] = " ".join(sub_tokens)
 	return extended_corpus
-
-
-# if __name__ == '__main__':
-# 	corpus = corpus.read_corpus("corpus_shuffled.csv")
-# 	extended_corpus = extract_surface_patterns(corpus, 1000)
-# 	vocabulary = get_vocabulary(extended_corpus, 'SURFACE_PATTERNS', 2)
-# 	vec = extract(extended_corpus[0], 'SURFACE_PATTERNS', vocabulary)
